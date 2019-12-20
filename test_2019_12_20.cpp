d3aaad13c956b45f861404ce76b12b9c8505411e
maoyanmm <804826526@qq.com>
2019-12-20 23:00:04 +0800

    练习了：二叉树的镜像

---
+++ test_2019_12_20.cpp
@@ -0,0 +1,28 @@
+truct TreeNode {
+        int val;
+            struct TreeNode *left;
+                struct TreeNode *right;
+                    TreeNode(int x) :
+                        val(x), left(NULL), right(NULL) {
+                                
+                        }
+
+};*/
+class Solution {
+    public:
+        void Mirror(TreeNode *pRoot) {
+                    if(pRoot == NULL)
+                    {
+                                    return;
+                                            
+                    }
+                            TreeNode* tmp = NULL;
+                                    tmp = pRoot->left;
+                                            pRoot->left = pRoot->right;
+                                                    pRoot->right = tmp;
+                                                            Mirror(pRoot->left);
+                                                                    Mirror(pRoot->right);
+                                                                        
+        }
+
+};
